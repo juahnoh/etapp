cac1f90e5d356da58804f4202049604f1be514dd
juahnoh <139471569+juahnoh@users.noreply.github.com>
2023-08-02 20:21:22 +0200

    수정버전

--- recording_screen.dart
+++ recording_screen.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:etapp/recording_screen2.dart';
+import 'package:flutter/services.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+
 
 class recordScreen extends StatefulWidget {
   const recordScreen({super.key});
@@ -10,6 +13,20 @@ class recordScreen extends StatefulWidget {
 }
 
 class _recordScreenState extends State<recordScreen> {
+  List<double>? _restingAccelerometerValues;
+  List<double>? _restingaccelerometerValues;
+  List<double>? _restinggyroscopeValues;
+  List<double>? _restingmagnetometerValues;
+  final _streamSubscriptions = <StreamSubscription<dynamic>>[];
+
+  List<String>? rAccelerometer;
+  List<String>? raccelerometer;
+  List<String>? rgyroscope;
+  List<String>? rmagnetometer;
+
+
+
+
   static const thirtySeconds = 5;
   int totalSeconds = thirtySeconds;
   bool isRunning = false;
@@ -53,10 +70,107 @@ class _recordScreenState extends State<recordScreen> {
     var duration = Duration(seconds: seconds);
     return duration.toString().substring(2,7);
   }
-
+  @override
+  void initState() {
+    super.initState();
+    _streamSubscriptions.add(
+      userAccelerometerEvents.listen(
+            (UserAccelerometerEvent event) {
+          setState(() {
+            _restingAccelerometerValues= <double>[event.x, event.y, event.z];
+          });
+        },
+        onError: (e) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const AlertDialog(
+                  title: Text("Sensor Not Found"),
+                  content: Text(
+                      "It seems that your device doesn't support Accelerometer Sensor"),
+                );
+              });
+        },
+        cancelOnError: true,
+      ),
+    );
+    _streamSubscriptions.add(
+      accelerometerEvents.listen(
+            (AccelerometerEvent event) {
+          setState(() {
+            _restingaccelerometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
+        onError: (e) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const AlertDialog(
+                  title: Text("Sensor Not Found"),
+                  content: Text(
+                      "It seems that your device doesn't support Gyroscope Sensor"),
+                );
+              });
+        },
+        cancelOnError: true,
+      ),
+    );
+    _streamSubscriptions.add(
+      gyroscopeEvents.listen(
+            (GyroscopeEvent event) {
+          setState(() {
+            _restinggyroscopeValues = <double>[event.x, event.y, event.z];
+          });
+        },
+        onError: (e) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const AlertDialog(
+                  title: Text("Sensor Not Found"),
+                  content: Text(
+                      "It seems that your device doesn't support User Accelerometer Sensor"),
+                );
+              });
+        },
+        cancelOnError: true,
+      ),
+    );
+    _streamSubscriptions.add(
+      magnetometerEvents.listen(
+            (MagnetometerEvent event) {
+          setState(() {
+            _restingmagnetometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
+        onError: (e) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const AlertDialog(
+                  title: Text("Sensor Not Found"),
+                  content: Text(
+                      "It seems that your device doesn't support Magnetometer Sensor"),
+                );
+              });
+        },
+        cancelOnError: true,
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    if(isRunning==true){
+      setState(() {
+         rAccelerometer = _restingAccelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+         raccelerometer = _restingaccelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+         rgyroscope = _restinggyroscopeValues?.map((double v) => v.toStringAsFixed(1)).toList();
+         rmagnetometer = _restingmagnetometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+      });
+    }else{
+    }
+    return
+      Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.orange,
         title: Text('Measuring resting tremor', style: Theme.of(context).textTheme.titleMedium),
@@ -140,6 +254,17 @@ class _recordScreenState extends State<recordScreen> {
                   )
                   ]
             ),),
+          Flexible(
+            flex: 1,
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('UserAccelerometer: $rAccelerometer'),
+              Text('Accelerometer: $raccelerometer'),
+              Text('Gyroscope: $rgyroscope'),
+              Text('Magnetometer: $rmagnetometer')
+            ],
+          ),)
         ],
       ),
     );
